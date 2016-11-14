17e78da59f01637767ada238d69da4938cb33561
Frode Jacobsen <frodejac@users.noreply.github.com>
2016-11-14 21:18:01 +0100

    Added LED convenience functions

--- include/led_control.h
+++ include/led_control.h
@@ -1,7 +1,18 @@
 #ifndef _LED_CONTROL_H_
 #define _LED_CONTROL_H_
 
+#ifndef STK
+#define LED_PORT gpioPortD
+#define LED_PIN0 4
+#define LED_PIN1 5
+#define LED_PIN2 6
+#define LED_PIN3 7
+
 int setupLED(void);
+int setLEDs(uint32_t mask);
+int toggleLEDs(uint32_t mask);
+
+#endif
 
 #endif
 
--- src/led_control.c
+++ src/led_control.c
@@ -1,5 +1,45 @@
 #include "em_gpio.h"
+#include "em_cmu.h"
+
+#include "led_control.h"
 
 int setupLED(void) {
+#ifndef STK
+	CMU_ClockEnable(cmuClock_GPIO, true);
+
+	/* Enable LED output */
+
+ 	  GPIO_PinModeSet(LED_PORT,         	/* Port */
+                  	LED_PIN0,         	/* Pin */
+                  	gpioModePushPull, 	/* Mode */
+                  	0);              	/* Output value */
+
+    GPIO_PinModeSet(LED_PORT,         	/* Port */
+                  	LED_PIN1,          	/* Pin */
+                  	gpioModePushPull, 	/* Mode */
+                  	0);              	/* Output value */
+
+    GPIO_PinModeSet(LED_PORT,         	/* Port */
+                  LED_PIN2,          	/* Pin */
+                  gpioModePushPull, 	/* Mode */
+                  0);	              	/* Output value */
+
+    GPIO_PinModeSet(LED_PORT,         	/* Port */
+                  LED_PIN3,          	/* Pin */
+                  gpioModePushPull, 	/* Mode */
+                  0);    				/* Output value */
+
+    GPIO_PortOutSet(LED_PORT, 0x0000);
+#endif
+	return 0;
+}
+
+int setLEDs(uint32_t mask) {
+#ifndef STK
+	/* Clear (set low) all outputs */
+	GPIO_PortOutClear(LED_PORT, 0xFFFFFFFF);
+	/* Set all outputs denoted by mask to high */
+	GPIO_PortOutSet(LED_PORT, mask);
+#endif
 	return 0;
 }
