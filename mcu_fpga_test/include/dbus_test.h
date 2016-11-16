001ff23009bede59cd9f4c97ef8228b2fa460bdf
Frode Jacobsen <frodejac@users.noreply.github.com>
2016-11-16 20:14:58 +0100

    Minor bugfixes.

--- mcu_fpga_test/include/dbus_test.h
+++ mcu_fpga_test/include/dbus_test.h
@@ -6,6 +6,6 @@ void display_bus_on_led_no_protocol(void);
 /* Test function for displaying the lowest half-byte on the DBUS on the LEDs while conforming to the protocol */
 void display_bus_on_led(void);
 /* Test function for displying the lowest half-byte on the DBUS on the LEDs while conforming to the protocol and checking that the signal is valid before reading */
-void display_bus_on_led_check_valid(void)
+void display_bus_on_led_check_valid(void);
 
 #endif
--- mcu_fpga_test/src/dbus_test.c
+++ mcu_fpga_test/src/dbus_test.c
@@ -4,10 +4,7 @@
 #include "leds.h"
 
 void display_bus_on_led_no_protocol(void) {
-	/* Get data */
-	uint32_t data = DBUS_get_data();
-	/* Display on LEDs */
-	set_LED(data << 4);
+	LEDS_update_all(DBUS_get_data() << 4);
 	/* Tell FPGA to update values */
 	DBUS_set_ACK();
 	DBUS_clear_ACK();
@@ -15,14 +12,13 @@ void display_bus_on_led_no_protocol(void) {
 
 void display_bus_on_led(void) {
 	/* Assume that function is called from interrupt -> No VALID checking. */
-	/* Get data */
-	uint16_t data = DBUS_read();
-	/* Display on LEDs */
-	set_LED(data << 4);
+	LEDS_update_all(DBUS_read() << 4);
 }
 
 void display_bus_on_led_check_valid(void) {
-	while (!DBUS_get_VALID()) {/* Wait for DBUS signals to become valid before proceeding. */}
-	uint16_t data = DBUS_read();
-	set_LED(data << 4);
+	/* Wait for DBUS signals to become valid before proceeding. */
+	while (!DBUS_get_VALID()) {
+		/* Do nothing */
+	}
+	LEDS_update_all(DBUS_read() << 4);
 }
