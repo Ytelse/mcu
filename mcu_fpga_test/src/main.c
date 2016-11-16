0114a5d80f894bc939b23cb5f584f610727ec654
Frode Jacobsen <frodejac@users.noreply.github.com>
2016-11-16 20:22:17 +0100

    Updated with inlined versions of most functions.

--- mcu_fpga_test/src/main.c
+++ mcu_fpga_test/src/main.c
@@ -11,7 +11,9 @@
 
 #include "usbcontrol.h"
 #include "leds.h"
-#include "fpga_comm.h"
+#include "dbus.h"
+#include "dbus_test.h"
+//#include "fpga_comm.h"
 
 /* UBUF is a macro for creating WORD-aligned uint8_t buffers */
 UBUF(img_buf0, BUFFERSIZE_SEND);
@@ -44,10 +46,11 @@ void switch_buf(void) {
 	buf_full = false;				/* New buffer is not full */
 	buf_rdy = false;				/* Inactive buffer is not ready */
 
-	resume_FPGA_comm();				/* We have a active buffer, so communiction with FPGA can resume */
+	//resume_FPGA_comm();				/* We have a active buffer, so communiction with FPGA can resume */
+	DBUS_resume();
 }
 
-void setup_ctrl(void) {
+void init_ctrl(void) {
 	buf_idx = 0;
 	buf_sel = 0;
 	buf_full = false;
@@ -55,16 +58,6 @@ void setup_ctrl(void) {
 	_wait = true;
 	_halt = false;
 }
-
-/*
- * TODO: Use __STATIC_INLINE keyword as much as possible when creating functions.
- * The __STATIC_INLINE keyword instructs the compiler to attempt to replace
- * the function call with the actual code of the function (if possible).
- * This is a way to increase speed of code drastically, as we do not have to
- * branch, create new stack, populate stack, branch back (with return values), populate
- * old stack and so on. This save many, many cycles, and is essential if we want the
- * data bus to be fast.
- */
 
 int main(void) {
 	CHIP_Init();
@@ -75,70 +68,32 @@ int main(void) {
 
 	/* Set up control variables */
 
-	setup_ctrl();
+	init_ctrl();
 
 	/* Initialize image buffers to 0 */
 	memset(img_buf0, 0, BUFFERSIZE_SEND);
 	memset(img_buf1, 0, BUFFERSIZE_SEND);
 
-	/* Setup LED pins */
-	setup_LED();
+	/* Initialize LED pins */
+	LEDS_init();
 	/* Setup USB stack */
+	/* TODO: Move initialization of USB stack from usbcontrol.c to here */
 	setup_USB();
-	/* Setup FPGA communication I/O pins */
-	setup_FPGA_comm();
+	/* Initialize DBUS for communication with FPGA */
+	DBUS_init();
 
 	while (_wait) { 
-		uint32_t led_mask = display_bus_on_led();
 		/* Wait for ready signal from USB host */
 		/* In test mode, output data bus on LEDs */
-		//set_ack_low();
-		set_LED(led_mask);
-		Delay(1);
-		set_LED(LEDS_ALL_OFF);
-		Delay(24);
-		set_LED(led_mask);
-		Delay(1);
-		set_LED(LEDS_ALL_OFF);
-		Delay(24);
-		set_LED(led_mask);
-		Delay(1);
-		set_LED(LEDS_ALL_OFF);
-		Delay(24);
-		set_LED(led_mask);
-		Delay(1);
-		set_LED(LEDS_ALL_OFF);
-		Delay(24);
-		set_LED(led_mask);
-		Delay(1);
-		set_LED(LEDS_ALL_OFF);
-		Delay(24);
-		set_LED(led_mask);
-		Delay(1);
-		set_LED(LEDS_ALL_OFF);
-		Delay(24);
-		set_LED(led_mask);
-		Delay(1);
-		set_LED(LEDS_ALL_OFF);
-		Delay(24);
-		set_LED(led_mask);
-		Delay(1);
-		set_LED(LEDS_ALL_OFF);
-		Delay(24);
-		/* Clear ACK */
-		// GPIO_PinOutClear(gpioPortC, 8);
-		// if (GPIO_PortInGet(DATA_BUS_PORT) & 0x00008000) { /* Get bus value */
-		// 	GPIO_PortOutSet(LED_PORT, LEDS_ALL_ON); /* Set leds */
-		// } else {
-		// 	GPIO_PortOutClear(LED_PORT, LEDS_ALL_ON); /* Set leds */
-		// }
-		// GPIO_PinOutSet(gpioPortC, 8); /* Set ACK */
+		display_bus_on_led_no_protocol();
 	};
 
 	/* Start communcation with FPGA */
-	start_FPGA_comm();
+	//start_FPGA_comm();
+	DBUS_start();
 
 	while(!_halt) {
+		/* TODO: Fix the run loop */
 		if (buf_full && buf_rdy) {
 			switch_buf();
 			notify_img_buf_ready();
@@ -146,6 +101,8 @@ int main(void) {
 			//set_LED(buf_idx << 4);
 		}
 	}
-	stop_FPGA_comm();
-	set_LED(LEDS_ALL_ON);
+	//stop_FPGA_comm();
+	DBUS_stop();
+	//set_LED(LEDS_ALL_ON);
+	LEDS_set_all();
 }
