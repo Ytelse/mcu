8aae1516a21f66f65125c1e0f813fda5739bc06d
Frode Jacobsen <frodejac@users.noreply.github.com>
2016-11-17 23:09:13 +0100

    Test for evaluating behaviour of APSR reserved bits.

--- mcu_fpga_test/src/main.c
+++ mcu_fpga_test/src/main.c
@@ -12,86 +12,66 @@
 #include "defs.h"
 
 #include "leds.h"
-
 #include "dbus.h"
+#include "mstate.h"
+
 #include "usbcallbacks.h"
 #include "usbdescriptors.h"
 
-#include "rtc_timing.h"
+//#include "rtc_timing.h"
 
 /* UBUF is a macro for creating WORD-aligned uint8_t buffers */
 UBUF(img_buf0, BUFFERSIZE_SEND);
 UBUF(img_buf1, BUFFERSIZE_SEND);
 
-/* BUFFER STATUS VARIABLES */
-volatile unsigned int 	buf_idx; 	/* Index of the first unwritten memory location of active buffer */
-volatile uint8_t 		buf_sel; 	/* Denotes which buffer is currently active, 0 -> buf0, 1 -> buf1 */
-volatile bool 			buf_full; 	/* Denotes whether active buffer is full or not */
-volatile bool			buf_rdy;	/* Denotes whether inactive buffer is finished being written over USB */
-
 volatile bool _wait, _halt;
 
 volatile uint32_t msTicks;
-volatile uint32_t bus_reads;
+// volatile uint32_t bus_reads;
 
 /* RTC Interrupt Handler */
 
-void RTC_IRQHandler(void) {
-	RTC_IntClear(RTC_IFC_COMP0);
+// void RTC_IRQHandler(void) {
+// 	RTC_IntClear(RTC_IFC_COMP0);
 
-	uint32_t led_mask = 0;
-	led_mask |= (bus_reads > 0x000E0000) ? LED0 : LEDS_NONE;
-	led_mask |= (bus_reads > 0x000C0000) ? LED1 : LEDS_NONE;
-	led_mask |= (bus_reads > 0x000A0000) ? LED2 : LEDS_NONE;
-	led_mask |= (bus_reads > 0x00080000) ? LED3 : LEDS_NONE;
+// 	uint32_t led_mask = 0;
+// 	led_mask |= (bus_reads > 0x000E0000) ? LED0 : LEDS_NONE;
+// 	led_mask |= (bus_reads > 0x000C0000) ? LED1 : LEDS_NONE;
+// 	led_mask |= (bus_reads > 0x000A0000) ? LED2 : LEDS_NONE;
+// 	led_mask |= (bus_reads > 0x00080000) ? LED3 : LEDS_NONE;
 
-	LEDS_update_all(led_mask);
-	bus_reads = 0;
-}
+// 	LEDS_update_all(led_mask);
+// 	bus_reads = 0;
+// }
 
 /* SystTick Handler */
 
-// void SysTick_Handler(void) {
-// 	msTicks++;       /* increment counter necessary in Delay()*/
-// }
+void SysTick_Handler(void) {
+	msTicks++;       /* increment counter necessary in Delay()*/
 
-// void Delay(uint32_t dlyTicks) {
-// 	uint32_t curTicks;
-
-// 	curTicks = msTicks;
-// 	while ((msTicks - curTicks) < dlyTicks) ;
-// }
-
-void switch_buf(void) {
-	buf_sel = (buf_sel) ? 0 : 1; 	/* Select the buffer that is not in use */
-	buf_idx = 0; 					/* Reset buf_idx */
-	buf_full = false;				/* New buffer is not full */
-	buf_rdy = false;				/* Inactive buffer is not ready */
-
-	//resume_FPGA_comm();				/* We have a active buffer, so communiction with FPGA can resume */
-	DBUS_resume();
 }
 
-void init_ctrl(void) {
-	buf_idx = 0;
-	buf_sel = 0;
-	buf_full = false;
-	buf_rdy = true;
-	_wait = true;
-	_halt = false;
-	bus_reads = 0;
+void Delay(uint32_t dlyTicks) {
+	uint32_t curTicks;
+	curTicks = msTicks;
+	while ((msTicks - curTicks) < dlyTicks) ;
 }
+
+// void switch_buf(void) {
+// 	buf_sel = (buf_sel) ? 0 : 1; 	/* Select the buffer that is not in use */
+// 	buf_idx = 0; 					/* Reset buf_idx */
+// 	buf_full = false;				/* New buffer is not full */
+// 	buf_rdy = false;				/* Inactive buffer is not ready */
+
+// 	DBUS_resume();
+// }
 
 int main(void) {
 	CHIP_Init();
 
 	CMU_ClockSelectSet(cmuClock_HF, cmuSelect_HFXO);
 
-	//if (SysTick_Config(CMU_ClockFreqGet(cmuClock_CORE) / 1000)) while (1) ;
-
-	/* Set up control variables */
-
-	init_ctrl();
+	if (SysTick_Config(CMU_ClockFreqGet(cmuClock_CORE) / 1000)) while (1) ;
 
 	/* Initialize image buffers to 0 */
 	memset(img_buf0, 0, BUFFERSIZE_SEND);
@@ -106,40 +86,64 @@ int main(void) {
 	/* Initialize DBUS for communication with FPGA */
 	DBUS_init();
 
-	/* Enable Real-time clock interrupt every 1 second */
-	setupRtc();
-
-	while (_wait) { 
-		/* Wait for ready signal from USB host */
-		/* In test mode, output data bus on LEDs */
-
-		if  (DBUS_get_data() + 1) {
-			bus_reads++;
-		}
-
-		DBUS_set_ACK();
-		DBUS_clear_ACK();
+	/* Set up control variables */
+	static MSTATE_init_struct_t mstate_initstruct = {
+		.mcuState	=	MSTATE_MCU_WAIT,
+		.bufSelect	=	MSTATE_BUF_SEL_0
 	};
 
-	/* Start communcation with FPGA */
-	//start_FPGA_comm();
-	DBUS_start();
+	MSTATE_init(&mstate_initstruct);
 
-	while(!_halt) {
-		/* TODO: Fix the run loop */
-		if (buf_full && buf_rdy) {
-			switch_buf();
-			if (!buf_sel) { /* Write opposite of currently active buffer */
-			  USBD_Write(EP_IN, img_buf1, BUFFERSIZE_SEND, dataSentCallback);
-			} else {
-			  USBD_Write(EP_IN, img_buf0, BUFFERSIZE_SEND, dataSentCallback);
-			}
-		} else {
-			//set_LED(buf_idx << 4);
-		}
+	/* Enable Real-time clock interrupt every 1 second */
+	//setupRtc();
+
+	LEDS_clear_all();
+
+	while (msTicks < 10000) {
+		/* Set LEDs to APSR[31:28] */ 
+		LEDS_update_all(__MSTATE_get_all() >> 28);
+		Delay(200);
 	}
-	//stop_FPGA_comm();
-	DBUS_stop();
-	//set_LED(LEDS_ALL_ON);
-	LEDS_set_all();
+
+	while (msTicks < 20000) {
+		/* Set LEDs to APSR[27:24] */
+		LEDS_update_all(__MSTATE_get_all() >> 24);
+		Delay(200);
+	}
+
+	while (msTicks < 30000) {
+		/* Set LEDs to APSR[23:20] */
+		LEDS_update_all(__MSTATE_get_all() >> 20);
+		Delay(200);
+	}
+
+	while (msTicks < 40000) {
+		/* Set LEDs to APSR[19:16] */
+		LEDS_update_all(__MSTATE_get_all() >> 16);
+		Delay(200);
+	}
+
+	while (msTicks < 50000) {
+		/* Set LEDs to APSR[15:12] */
+		LEDS_update_all(__MSTATE_get_all() >> 12);
+		Delay(200);
+	}
+
+	while (msTicks < 60000) {
+		/* Set LEDs to APSR[11:8] */
+		LEDS_update_all(__MSTATE_get_all() >> 8);
+		Delay(200);
+	}
+
+	while (msTicks < 70000) {
+		/* Set LEDs to APSR[7:4] */
+		LEDS_update_all(__MSTATE_get_all() >> 4);
+		Delay(200);
+	}
+
+	while (msTicks < 80000) {
+		/* Set LEDs to APSR[3:0] */
+		LEDS_update_all(__MSTATE_get_all() >> 0);
+		Delay(200);
+	}
 }
